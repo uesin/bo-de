36c25c66b9edde3210e76c1280bdcd4cc5b9e2d7
uesin <kazukihana87@gmail.com>
2020-01-16 02:26:46 +0900

    ユーザープロフィール画面の作成
    ユーザーコントローラーを作成
    
    showアクションの作成
    
    マイページのビューを作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,6 +29,6 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content).merge(user_id: current_user.id)
   end
 end
