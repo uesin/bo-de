fcb89cf0abadae0665a017280c30b0e3de039ce8
uesin <kazukihana87@gmail.com>
2020-01-21 15:51:59 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ module BoDe
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.time_zone = 'Asia/Tokyo'
-    config.i18n.default_locale = :ja
       
   end
   
