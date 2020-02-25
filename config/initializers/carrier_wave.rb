57cbc2c0db26f9e59f1e134218e3e725c5af973b
uesin <kazukihana87@gmail.com>
2020-02-25 18:38:47 +0900

    Update carrier_wave.rb

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,6 +1,4 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
+
 
 
 if Rails.env.production?
@@ -12,7 +10,6 @@ CarrierWave.configure do |config|
     region:                => ENV['S3_REGION'], 
     aws_access_key_id:    => ENV['S3_ACCESS_KEY'],
     aws_secret_access_key: => ENV['S3_SECRET_KEY'],
-    path_style: true
   }
   config.fog_directory     =  ENV['S3_BUCKET']
     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } 
